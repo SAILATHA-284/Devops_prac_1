7127fed1b6ee235f7fc2b960c2ac83ae5de93cb3
SAILATHA-284 <sai.she.rt22@dypatil.edu>
2025-04-29 18:41:09 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test{
+    public static void main(String[] arg){
+        int a =5;
+        int b=7;
+        int c=a+b;
+        System.out.println("Addition of 5 and 7 is:");
+        System.out.println(c);
+    }
+}
